5e50d8b2e7b98d307b93b963b5c2df0e9a10c8bb
sinou <srinivasane.arav@gmail.com>
2014-09-02 19:33:02 -0400

    kill-some-time push

---
+++ src/com/srini/mathandstats/ParanthesisProblemo.java
@@ -0,0 +1,26 @@
+package com.srini.mathandstats;
+
+public class ParanthesisProblemo {
+	public static void main(String args[]){
+		solveForN(3, 0, 0, "");
+	}
+	public static void solveForN(int n, int leftCount, int rightCount, String output){
+		if(n == 0){
+			return;
+		}
+		
+		if(n == leftCount && n == rightCount){
+			System.out.println(output);
+		}
+		if(leftCount < n ){
+			output += '{';
+			solveForN(n, leftCount + 1, rightCount, output);
+			output = output.substring(0, output.length() - 1);
+		}
+		if(rightCount < leftCount){
+			output += '}';
+			solveForN(n, leftCount, rightCount + 1, output);
+			output = output.substring(0, output.length() - 1);
+		}
+	}
+}

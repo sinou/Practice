0914c5e1ba6cab530eba4ed4369c79269534073f
sinou <srinivasane.arav@gmail.com>
2014-09-06 22:22:19 -0400

    just 2 problems

---
+++ src/com/srini/trees/CommonAncestor.java
@@ -0,0 +1,34 @@
+package com.srini.trees;
+
+public class CommonAncestor {
+	
+	public static void main(String args[]){
+		
+	}
+	
+	public static Node commonAncestor(Node root, Node t1, Node t2){
+		
+		if(covers(root.leftChild, t1) && covers(root.leftChild, t2)){
+			return commonAncestor(root.leftChild, t1, t2);
+		}
+		if(covers(root.rightChild, t1) && covers(root.rightChild, t2)){
+			return commonAncestor(root.rightChild, t1, t2);
+		}
+		return root;
+		
+	}
+	
+	public static boolean covers(Node root, Node child){
+		
+		if(root == null){
+			return false;
+		}
+		if(root == child){
+			return true;
+		}
+
+		return (covers(root.leftChild, child) || covers(root.leftChild, child));
+		
+	}
+	
+}
---
+++ src/com/srini/trees/SubTree.java
@@ -0,0 +1,43 @@
+package com.srini.trees;
+
+public class SubTree {
+	
+	public static void main(String args[]){
+		
+	}
+	
+	static boolean containsTree(Node t1, Node t2){
+		if(t2 == null){
+			return true;
+		}
+		
+		return subTree(t1, t2);
+	}
+	
+	static boolean subTree(Node t1, Node t2){
+		if(t1 == null){
+			return false;
+		}
+		if(t1.data == t2.data){
+			return matchTrees(t1, t2);
+		}
+		
+		return (matchTrees(t1.leftChild, t2) || matchTrees(t1.rightChild, t2));
+	}
+	
+	static boolean matchTrees(Node t1, Node t2){
+		
+		if(t1 == null && t2 == null){
+			return true;
+		}
+		if(t1 == null || t2 == null){
+			return false;
+		}
+		if(t1.data == t2.data){
+			return false;
+		}
+		
+		return (matchTrees(t1.leftChild, t2.leftChild) && matchTrees(t1.rightChild, t2.rightChild));
+		
+	}
+}

4f703a949fdec5aad8b001127af0b355630f4817
sinou <srinivasane.arav@gmail.com>
2014-10-11 15:07:39 -0400

    l&s

---
+++ src/com/srini/misc/LookAndSay.java
@@ -0,0 +1,31 @@
+package com.srini.misc;
+
+public class LookAndSay {
+	
+	public static void main(String[] args){
+		String num = "1"; 
+	 
+		for (int i=1;i<=10;i++) {
+			U.prtl(num);
+			num = doLookAndSay(num);             
+		}
+	}
+
+	public static String doLookAndSay(String number){
+		StringBuilder result = new StringBuilder();
+		char trailing = number.charAt(0);
+		number = number.substring(1) + " ";
+		int times = 1;
+		for(char leading: number.toCharArray()){
+			if(trailing != leading){
+				result.append(times + "" + trailing);
+				times = 1;
+				trailing = leading;
+			}else{
+				times++;
+			}
+		}
+		return result.toString();
+	}
+	
+}
